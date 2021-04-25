c4e7834599d8a464230c7789cfe463d9bba07309
王彦彬 <wyb@dataqin.com>
2021-04-25 10:34:29 +0800

    kt化，部分代码精简

--- app/src/main/java/com/shuniuyun/material/activity/BottomSheetDialogActivity.kt
+++ app/src/main/java/com/shuniuyun/material/activity/BottomSheetDialogActivity.kt
@@ -1,18 +1,15 @@
 package com.shuniuyun.material.activity
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.shuniuyun.material.R
+import com.shuniuyun.material.utils.BottomSheetDialogHelper
 
 /**
  * author:wyb
  */
 class BottomSheetDialogActivity : BaseActivity(), View.OnClickListener {
-    private val mBottomSheetDialog by lazy { BottomSheetDialog(this) }
+//    private val mBottomSheetDialog by lazy { BottomSheetDialog(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,28 +18,30 @@ class BottomSheetDialogActivity : BaseActivity(), View.OnClickListener {
         initEvent()
     }
 
-    private fun initView(){
-        mBottomSheetDialog.setContentView(LayoutInflater.from(this).inflate(R.layout.view_bottomsheetdialog, null, false))
+    private fun initView() {
+        BottomSheetDialogHelper.initialize(this, R.layout.view_bottomsheetdialog)
+//        mBottomSheetDialog.setContentView(LayoutInflater.from(this).inflate(R.layout.view_bottomsheetdialog, null, false))
     }
 
-    private fun initEvent(){
-        val bottomSheetBehavior = BottomSheetBehavior.from(mBottomSheetDialog.delegate.findViewById(R.id.design_bottom_sheet)!!)
-        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-                    mBottomSheetDialog.dismiss()
-                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-                }
-            }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
-        })
+    private fun initEvent() {
+//        val bottomSheetBehavior = BottomSheetBehavior.from(mBottomSheetDialog.delegate.findViewById(R.id.design_bottom_sheet)!!)
+//        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetCallback() {
+//            override fun onStateChanged(bottomSheet: View, newState: Int) {
+//                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+//                    mBottomSheetDialog.dismiss()
+//                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//                }
+//            }
+//
+//            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+//        })
     }
 
     override fun onClick(v: View) {
-        if (mBottomSheetDialog.isShowing) {
-            mBottomSheetDialog.dismiss()
-        } else mBottomSheetDialog.show()
+        if (BottomSheetDialogHelper.isShowing()) BottomSheetDialogHelper.hidden() else BottomSheetDialogHelper.shown()
+//        if (mBottomSheetDialog.isShowing) {
+//            mBottomSheetDialog.dismiss()
+//        } else mBottomSheetDialog.show()
     }
 
 }
---
+++ app/src/main/java/com/shuniuyun/material/utils/BottomSheetDialogHelper.kt
@@ -0,0 +1,45 @@
+package com.shuniuyun.material.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.shuniuyun.material.R
+
+/**
+ *  Created by wangyanbin
+ *  底部弹出窗体工具类
+ */
+@SuppressLint("StaticFieldLeak")
+object BottomSheetDialogHelper {
+    private var sheetDialog: BottomSheetDialog? = null
+
+    fun initialize(context: Context, resource: Int) {
+        sheetDialog = BottomSheetDialog(context)
+        sheetDialog?.setContentView(LayoutInflater.from(context).inflate(resource, null, false))
+        val behavior = BottomSheetBehavior.from(sheetDialog?.delegate?.findViewById(R.id.design_bottom_sheet)!!)
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    sheetDialog?.dismiss()
+                    behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
+    }
+
+    fun isShowing() = sheetDialog?.isShowing ?: false
+
+    fun shown() {
+        if (!isShowing()) sheetDialog?.show()
+    }
+
+    fun hidden() {
+        if (isShowing()) sheetDialog?.dismiss()
+    }
+
+}
