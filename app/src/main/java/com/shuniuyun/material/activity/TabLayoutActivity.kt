9fd3519b5f93bcdd9cb7965230b99197ba7b8fb6
王彦彬 <wyb@dataqin.com>
2021-05-25 16:12:33 +0800

    kt化，部分代码精简

--- app/src/main/java/com/shuniuyun/material/activity/TabLayoutActivity.kt
+++ app/src/main/java/com/shuniuyun/material/activity/TabLayoutActivity.kt
@@ -4,8 +4,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
@@ -20,6 +24,8 @@ class TabLayoutActivity : BaseActivity() {
     private val toolbar by lazy { findViewById<Toolbar>(R.id.toolBar) }
     private val tabLayout by lazy { findViewById<TabLayout>(R.id.tabLayout) }
     private val viewPager by lazy { findViewById<ViewPager>(R.id.viewpager) }
+
+    private var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,6 +85,38 @@ class TabLayoutActivity : BaseActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        //搜索的配置参数
+        val searchMenuItem = menu.findItem(R.id.action_search)
+//        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = searchMenuItem.actionView as SearchView
+//        searchView?.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        //搜索图标是否显示在搜索框内
+        searchView?.setIconifiedByDefault(true)
+        //设置搜索框展开时是否显示提交按钮，可不显示
+        searchView?.isSubmitButtonEnabled = true
+        //让键盘的回车键设置成搜索
+        searchView?.imeOptions = EditorInfo.IME_ACTION_SEARCH
+//        //搜索框是否展开，false表示展开
+//        searchView?.setIconified(false)
+//        //获取焦点
+//        searchView?.setFocusable(true)
+//        searchView?.requestFocusFromTouch()
+        //设置提示词
+        searchView?.queryHint = "请输入关键字"
+        //设置输入框文字颜色
+        val editText = searchView?.findViewById(androidx.appcompat.R.id.search_src_text) as EditText
+        editText.setHintTextColor(ContextCompat.getColor(this, R.color.white))
+        editText.setTextColor(ContextCompat.getColor(this, R.color.white))
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            //让键盘的回车键设置成搜索
+            searchView?.imeOptions = EditorInfo.IME_ACTION_SEARCH
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                //完成自己的事件
+                Toast.makeText(this, editText.text, Toast.LENGTH_SHORT).show()
+                searchView?.onActionViewCollapsed()
+            }
+            false
+        }
         return true
     }
 
