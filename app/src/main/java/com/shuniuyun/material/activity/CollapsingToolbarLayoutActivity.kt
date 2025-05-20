0a64eb90b1a2c09dde6bf478119ebf1506edca33
wangyanbin <wangyanbin_29@126.com>
2025-05-20 16:48:19 +0800

    更新，替换

--- app/src/main/java/com/shuniuyun/material/activity/CollapsingToolbarLayoutActivity.kt
+++ app/src/main/java/com/shuniuyun/material/activity/CollapsingToolbarLayoutActivity.kt
@@ -1,9 +1,12 @@
 package com.shuniuyun.material.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
 import com.shuniuyun.material.R
+import kotlin.math.abs
 
 /**
  * author:wyb
@@ -15,6 +18,23 @@ class CollapsingToolbarLayoutActivity : AppCompatActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collapsingtoolbarlayout)
+        val appbar = findViewById<AppBarLayout>(R.id.appbar)
+        appbar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            private var isHide = false
+            override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+                val needHide = abs(verticalOffset) < (appBarLayout?.totalScrollRange ?: 0)
+                if (needHide != isHide) {
+                    isHide = needHide
+                    //展开
+                    if (needHide) {
+                        Log.e("wyb","展开")
+                        //折起
+                    } else {
+                        Log.e("wyb","折起")
+                    }
+                }
+            }
+        })
     }
 
 }
